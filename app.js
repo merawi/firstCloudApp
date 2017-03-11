cf894fdba0754ba94f5d8f727e98bb34df2265ab
root <root@alpha.localdomain>
2017-03-11 17:49:02 +0000

    my first app

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ var app  = express();
 app.get('/', function(req,res,next){
 
 	res.send('hello world , wwelcome to my first cloud app');
-});
+}); 
 
 app.listen(8080, function(){
 	console.log('server is listening at port: 8080');
